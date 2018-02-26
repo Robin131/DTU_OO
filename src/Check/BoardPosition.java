24904b1bfb75b457eee07a3feef2a632976387e9
Robin131 <824009273@qq.com>
2018-02-26 23:15:33 +0100

    fix color

--- src/Check/BoardPosition.java
+++ src/Check/BoardPosition.java
@@ -2,17 +2,6 @@ package Check;
 
 import Check.Board.Direction;
 
-enum Color{
-	WHITE("2"), BLACK("1"), NONE(" "), INVALID("-1");
-	String num = null;
-	private Color(String num) {
-		this.num = num;
-	}
-	public String getPlayerNum() {
-		return num;
-	}
-}
-
 public class BoardPosition extends Position{
 	
 	private Color playerColor;
