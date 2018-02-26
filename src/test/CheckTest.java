60fef777c84731a164d598a5c17c98f33656c643
Robin131 <824009273@qq.com>
2018-02-26 21:16:01 +0100

    add test

--- src/test/CheckTest.java
+++ src/test/CheckTest.java
@@ -2,17 +2,27 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Scanner;
+
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-import core.FixedCapacityStackOfStrings;
+import Check.Board;
+import Check.Color;
+import Check.Player;
+import Check.Position;
 
 class CheckTest {
 
 	@Before
 	public void initialTest() 
 	{
-		stack = new FixedCapacityStackOfStrings(5);
+		Board board = new Board();
+		Player player1 = new Player(Color.BLACK);
+		Player player2 = new Player(Color.WHITE);
+		public static Position oldPosition = new Position(0, 0);
+		public static Position newPosition = new Position(0, 0);
+		public static Scanner input = new Scanner(System.in);
 	}
 
 }
